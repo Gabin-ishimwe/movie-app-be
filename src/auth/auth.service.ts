c6c03d05f37fb45deb931bbf1e7995f5e2bffb1c
gabin-AF <gabin@augmented-future.com>
2022-08-22 15:07:29 +0200

    rdatabase connectivity

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,42 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { AuthDto } from './dto';
+import * as argon2 from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signUp(dto: AuthDto) {
-    return { msg: 'I sign up....' };
+  async signUp(dto: AuthDto) {
+    try {
+      const { email, password } = dto;
+      const hashPassword = await argon2.hash(password);
+      const user = await this.prisma.user.create({
+        data: {
+          firstname: '',
+          lastname: '',
+          email,
+          password: hashPassword,
+        },
+      });
+      return user;
+    } catch (error) {
+      console.log(error);
+    }
   }
-  signIn() {
+  async signIn(dto: AuthDto) {
+    const { email, password } = dto;
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      throw new ForbiddenException('User arleady exist');
+    }
+    const confirmPassword = await argon2.verify(user.password, password);
+    if (!confirmPassword) {
+      throw new ForbiddenException('Incorrect credentials');
+    }
     return { msg: 'I sign In...' };
   }
 }
--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,20 +1,9 @@
 import { Global, Module } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
 import { PrismaService } from './prisma.service';
 
 @Global()
 @Module({
-  exports: [PrismaService],
   providers: [PrismaService],
+  exports: [PrismaService],
 })
-export class PrismaModule extends PrismaClient {
-  constructor() {
-    super({
-      datasources: {
-        db: {
-          url: 'postgresql://andelarwanda:andela123@localhost:5432/nestJsDb',
-        },
-      },
-    });
-  }
-}
+export class PrismaModule {}
